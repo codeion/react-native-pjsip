bb1a4031a5e0a76851c6e23ac00dabc540340ef6
Andrej Savic <andrej@codeion.com>
2023-12-03 08:07:41 +0100

    chore(libs): remove android libc++_shared.so

--- libs.sh
+++ libs.sh
@@ -5,6 +5,7 @@ VERSION="v2.9.1"
 URL="https://github.com/codeion/react-native-pjsip-builder/releases/download/${VERSION}/release.tar.gz"
 LOCK=".libs.lock"
 DEST=".libs.tar.gz"
+LIBS="android/src/main/jniLibs"
 DOWNLOAD=true
 
 if ! type "curl" > /dev/null; then
@@ -27,6 +28,10 @@ fi
 if [ "$DOWNLOAD" = true ]; then
     curl -L --silent "${URL}" -o "${DEST}"
     tar -xvf "${DEST}"
+    rm "${LIBS}/arm64-v8a/libc++_shared.so"
+    rm "${LIBS}/armeabi-v7a/libc++_shared.so"
+    rm "${LIBS}/x86/libc++_shared.so"
+    rm "${LIBS}/x86_64/libc++_shared.so"
     rm -f "${DEST}"
 
     echo "${VERSION}" > ${LOCK}
